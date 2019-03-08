0d0320923f4989d26c4c9fab890758b7f2885f18
Doug MacKenzie <douglmackenzie@gmail.com>
2019-03-09 00:20:20 +1100

    Add primitive prev & next buttons

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -4,14 +4,42 @@ import PropTypes from "prop-types";
 const Pagination = ({ itemsPerPage, children }) => {
   const [currentPage, setCurrentPage] = React.useState(1);
 
-  const offset = (currentPage - 1) * itemsPerPage;
-  const total = offset + itemsPerPage;
-
-  return children.slice(offset, total);
+  return (
+    <>
+      <PaginationItems currentPage={currentPage} itemsPerPage={itemsPerPage}>
+        {children}
+      </PaginationItems>
+      <PaginationButtons
+        currentPage={currentPage}
+        setCurrentPage={setCurrentPage}
+      />
+    </>
+  );
 };
 
 Pagination.propTypes = {
   itemsPerPage: PropTypes.number.isRequired
+};
+
+/**
+ * Displays the current page of items
+ */
+const PaginationItems = ({ currentPage, itemsPerPage, children }) => {
+  const offset = (currentPage - 1) * itemsPerPage;
+  const total = offset + itemsPerPage;
+  return children.slice(offset, total);
+};
+
+/**
+ * Displays the pagination buttons
+ */
+const PaginationButtons = ({ currentPage, setCurrentPage }) => {
+  return (
+    <>
+      <button onClick={() => setCurrentPage(currentPage - 1)}>Prev</button>
+      <button onClick={() => setCurrentPage(currentPage + 1)}>Next</button>
+    </>
+  );
 };
 
 export default Pagination;
--- stories/index.js
+++ stories/index.js
@@ -3,7 +3,7 @@ import { storiesOf } from "@storybook/react";
 import Pagination from "../src/components/Pagination";
 
 storiesOf("Pagination", module).add("basic example", () => (
-  <Pagination itemsPerPage="2">
+  <Pagination itemsPerPage={2}>
     <div>Item 1</div>
     <div>Item 2</div>
     <div>Item 3</div>
