07e201d38c1699aaea843e59285a05b506cd8530
Doug MacKenzie <douglmackenzie@gmail.com>
2019-03-09 22:47:07 +1100

    Add primitive version of numbered buttons

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -45,23 +45,42 @@ const PaginationButtons = ({
   const nextOffset = currentPage * itemsPerPage;
   const prevDisabled = prevOffset <= 0 ? "disabled" : "";
   const nextDisabled = nextOffset >= itemLength ? "disabled" : "";
+  const pageCount = itemLength / itemsPerPage;
 
   return (
     <>
-      <button
-        onClick={() => setCurrentPage(currentPage - 1)}
+      <EndButton
         disabled={prevDisabled}
+        onClick={() => setCurrentPage(currentPage - 1)}
       >
         Prev
-      </button>
-      <button
-        onClick={() => setCurrentPage(currentPage + 1)}
+      </EndButton>
+
+      <NumberedButtons pageCount={pageCount} setCurrentPage={setCurrentPage} />
+
+      <EndButton
         disabled={nextDisabled}
+        onClick={() => setCurrentPage(currentPage + 1)}
       >
         Next
-      </button>
+      </EndButton>
     </>
   );
+};
+
+const EndButton = ({ disabled, onClick, children }) => {
+  return (
+    <button onClick={onClick} disabled={disabled}>
+      {children}
+    </button>
+  );
+};
+
+const NumberedButtons = ({ pageCount, setCurrentPage }) => {
+  return [...Array(pageCount)].map((derp, index) => {
+    const pageNum = index + 1;
+    return <button onClick={() => setCurrentPage(pageNum)}>{pageNum}</button>;
+  });
 };
 
 export default Pagination;
