7b69f2a1cbbc9e5e9ee9cfa2ecda639cff1efde6
Doug MacKenzie <douglmackenzie@gmail.com>
2019-03-22 13:09:42 +1100

    Bundle the library using webpack and babel

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require("path");
+
+module.exports = {
+  mode: "production",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "lib"),
+    filename: "index.js",
+    library: "StatefulPagination",
+    libraryTarget: "umd"
+  },
+  externals: [
+    {
+      react: {
+        root: "React",
+        amd: "react",
+        commonjs: "react",
+        commonjs2: "react"
+      }
+    }
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /(node_modules)/,
+        use: "babel-loader"
+      }
+    ]
+  }
+};
